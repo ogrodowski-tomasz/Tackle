eb857cf762685936d698aeed55a0f2fa325ae5a8
Tomasz Ogrodowski <tom.ogrodowski@gmail.com>
2025-07-04 13:38:24 +0200

    Initial Commit

---
+++ Tackle/TackleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TackleApp.swift
+//  Tackle
+//
+//  Created by itcraft on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TackleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
